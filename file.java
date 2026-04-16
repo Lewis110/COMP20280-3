c11f52993363f390c618919ba6bc760d65ecc454
Liam O'Connor <oconnorliam360@gmail.com>
2026-04-16 14:21:05 +0100

    added py to create graphs

--- file.java
+++ file.java
@@ -8,4 +8,13 @@ public class file {
 git add .
 git commit -m "finished writing simple tests - still need all benchmarks"
 git push
+
+
+echo "# COMP20280-3" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Lewis110/COMP20280-3
+git pull -u origin main
  */
